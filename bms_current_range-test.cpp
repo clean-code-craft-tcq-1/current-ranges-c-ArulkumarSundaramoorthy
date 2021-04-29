5a67101e2ae2297ff08ac22a7a609bbf679dba9f
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-29 15:03:42 +0530

    Create bms_current_range-test.cpp

---
+++ bms_current_range-test.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "bms_current_ranges.h"
+#include "stdlib.h"
+#include "math.h"
+
+TEST_CASE("Test case to check NULL values")
+{
+    float readingSet[] = NULL;
+    int setLength = sizeof(readingSet) / sizeof(readingSet[0]);
+    int testStatus  = checkAndCountCurrentRanges(readingSet, setLength);
+    REQUIRE(testStatus == 1);
+}
