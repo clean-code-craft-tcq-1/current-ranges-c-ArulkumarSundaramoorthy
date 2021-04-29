5957e21ee2fcf470d1b318eb855960d15e0471bf
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-29 15:46:32 +0530

    Update bms_current_range-test.cpp

--- bms_current_range-test.cpp
+++ bms_current_range-test.cpp
@@ -7,7 +7,7 @@
 
 TEST_CASE("1. Fail Test case to check NULL values")
 {
-    float readingSet[] = {3, 3, 5, 4, 10, 11, 12};
+    int readingSet[] = {3, 3, 5, 4, 10, 11, 12};
     int setLength = sizeof(readingSet) / sizeof(readingSet[0]);
     Status testStatus  = checkAndCountCurrentRanges(NULL, setLength);
     REQUIRE(testStatus == TEST_FAIL);
