657a8256125af4d26f79a78c7a129d0395d99840
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-29 15:46:55 +0530

    Update bms_current_range.c

--- bms_current_range.c
+++ bms_current_range.c
@@ -2,7 +2,7 @@
 #include "bms_current_range.h"
 #include "math.h"
 
-Status checkAndCountCurrentRanges(const float* readingSet, int setLength)
+Status checkAndCountCurrentRanges(int * readingSet, int setLength)
 {
   return 0;
 }
